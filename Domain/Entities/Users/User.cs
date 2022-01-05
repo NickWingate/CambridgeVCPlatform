2f7554d7610b93b0ae490f32a881feee1633881e
NickWingate <nicholas.wingate03@gmail.com>
2022-01-05 15:33:52 +0000

    Add role and user

--- Domain/Entities/Users/User.cs
+++ Domain/Entities/Users/User.cs
@@ -4,7 +4,24 @@ namespace Domain.Entities.Users
 {
 	public class User
 	{
-		public string Name { get; set; }
-		public List<string> Roles { get; set; }
+		public int Id { get; set; }
+ 
+		public string UserName { get; set; }
+ 
+		public string NormalizedUserName { get; set; }
+ 
+		public string Email { get; set; }
+ 
+		public string NormalizedEmail { get; set; }
+ 
+		public bool EmailConfirmed { get; set; }
+ 
+		public string PasswordHash { get; set; }
+ 
+		public string PhoneNumber { get; set; }
+ 
+		public bool PhoneNumberConfirmed { get; set; }
+ 
+		public bool TwoFactorEnabled { get; set; }
 	}
 }
