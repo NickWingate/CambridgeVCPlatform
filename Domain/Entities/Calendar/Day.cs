5227a08679a3c92be0e423692f4ee73c9633afc4
Nick Wingate <nicholas.wingate03@gmail.com>
2022-01-05 15:27:49 +0000

    Add XamarinUi and Day entity

---
+++ Domain/Entities/Calendar/Day.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using Domain.Entities.Sessions;
+
+namespace Domain.Entities.Calendar
+{
+	public class Day
+	{
+		public List<Session> Sessions { get; set; }
+		public DateOnly Date { get; set; }
+	}
+}
---
+++ XamarinUi/XamarinUi/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using XamarinUi.Models;
+using XamarinUi.ViewModels;
+
+namespace XamarinUi.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
